7901fb23e051a796fb4516f312f6d541a61c1d35
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-08-24 13:29:11 +0100

    fixed some permissions issues

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const roleMiddleware = require('../middlewares/roleMiddleware');
 
 router.get('/', authMiddleware, roleMiddleware(['SuperAdmin']), userController.getUsers);
-router.get('/:id', authMiddleware, roleMiddleware(['SuperAdmin']), userController.getUserById);
+router.get('/:id', authMiddleware, userController.getUserById);
 router.put('/:id', authMiddleware, roleMiddleware(['SuperAdmin']), userController.updateUser);
 router.delete('/:id', authMiddleware, roleMiddleware(['SuperAdmin']), userController.deleteUser);
 
