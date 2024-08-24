2122d81941cf837b56a3c78b810f089c369c87f5
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-08-24 14:16:39 +0100

    changed some functionnalities

--- prov_app/src/app/my-servers/my-servers.component.ts
+++ prov_app/src/app/my-servers/my-servers.component.ts
@@ -2,12 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ServerService } from '../services/server.service';
 
+export interface EnvironmentStyle {
+  icon: string;
+  color: string;
+}
+
+export const environmentStyles: { [key: string]: EnvironmentStyle } = {
+  'developing': { icon: 'fa-code-branch', color: '#66BB6A' }, // Light Green
+  'testing': { icon: 'fa-check-double', color: '#42A5F5' }, // Bright Yellow
+  'staging': { icon: 'fa-cogs', color: '#FF9800' }, // Amber
+  'production': { icon: 'fa-shield-alt', color: '#FF5252' } // Bright Red
+};
+
 @Component({
   selector: 'app-my-servers',
   templateUrl: './my-servers.component.html',
   styleUrl: './my-servers.component.scss'
 })
+
+
 export class MyServersComponent implements OnInit {
+
+  getEnvironmentStyle(environmentType: string): EnvironmentStyle {
+    return environmentStyles[environmentType] || { icon: 'fa-question-circle', color: '#9E9E9E' }; // Default: gray question mark
+  }
 
   items: any[] = [];
 
