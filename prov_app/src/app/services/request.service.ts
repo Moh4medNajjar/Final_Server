8ae7a6bbc7472b76a43b2fbcf8b35d88174c7ef8
Moh4medNajjar <najjarmohamed443@gmail.com>
2024-08-30 08:46:02 +0100

    fixed ip address

--- prov_app/src/app/services/request.service.ts
+++ prov_app/src/app/services/request.service.ts
@@ -8,7 +8,7 @@ import { catchError, Observable, tap, throwError } from 'rxjs';
 })
 export class RequestService {
 
-  private apiUrl = 'http://localhost:3000/api/requests';
+  private apiUrl = 'http://4.245.13.187:3000/api/requests';
 
   constructor(private http: HttpClient, private authService: AuthService) { }
 
